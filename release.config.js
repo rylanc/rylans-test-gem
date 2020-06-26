b104513c4ba5e3cf113cb7380c80d50d54884900
Rylan Collins <rylan@gusto.com>
2020-06-25 19:52:40 -0700

    Remove gemHost

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['semantic-release-rubygem', { gemHost: 'https://rubygems.org' }],
+    ['semantic-release-rubygem'],
     '@semantic-release/github',
   ],
 };
